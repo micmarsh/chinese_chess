0c60733c230373cc6ae51d65bd907372a6e34fdc
micmarsh <michael.marsh42@gmail.com>
2014-01-19 15:13:39 -0500

    check tests looking hot

--- spec/move_spec.js
+++ spec/move_spec.js
@@ -19,5 +19,25 @@ describe('MovablePosition', function() {
 
         var noMove = position.canMove('0,0','0,7');
         expect(noMove).toBe(false);
+
     });
+
+    it('accounts for check', function () {
+        //move cannon all up in black's grille
+        position.makeMove('1,2', '1,9');
+
+        //try to move elephant in a way that cause check
+        var elephant = position.canMove('2,9','4,7');
+        expect(elephant).toBe(false);
+
+        //same deal with an advisor
+        var advisor = position.canMove('3,9','4,8');
+        expect(advisor).toBe(false);
+
+        //chariot can capture cannon, tho
+        var chariot = position.canMove('0,9','1,9');
+        expect(chariot).toBe(true);
+
+    })
+
 })
